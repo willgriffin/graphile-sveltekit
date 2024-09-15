b3bb18353e70114271bc584a93ad7c2a86bf4e74
will griffin <willgriffin@gmail.com>
2024-09-15 12:39:59 -0600

    feat: example query in sample page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <script lang="ts">
+import { onMount } from 'svelte';
+
+const query = `
+  query {
+    allUsers {
+      nodes {
+        id
+        name
+      }
+    }
+  }
+`;
+
+let users: { id: string; name: string }[] = [];
+let loading = true;
+
+async function fetchUsers() {
+  const response = await fetch('/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const result = await response.json();
+  users = result.data.allUsers.nodes;
+}
+
+onMount(() => {
+  fetchUsers().then(() => {
+    loading = false;
+  });
+});
+</script>
+
+{#if loading}
+  <p>Loading users...</p>
+{:else}
+  <ul>
+    {#each users as user}
+      <li>{user.name} (ID: {user.id})</li>
+    {/each}
+  </ul>
+{/if} -->
+
+<p>Hi</p>
